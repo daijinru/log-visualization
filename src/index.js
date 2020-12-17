7ec6e3895cb27c9c3d666d0fc538d5102e166caa
daijinru <jeocat@163.com>
2020-12-17 10:07:55 +0800

    U 箭头动效

--- src/index.js
+++ src/index.js
@@ -22,14 +22,28 @@ class App {
   }
 
   setEvents () {
+    // 单条日志点击事件和动效
     document.getElementById('log-visual-app').addEventListener('click', e => {
-      console.info(e);
       if (e.target.className === 'log-console-item-msg-text') {
+        // 日志详情展开
         const nextElementSibling = e.target.parentElement.nextElementSibling;
         if (nextElementSibling && nextElementSibling.classList.contains('collapsed')) {
           nextElementSibling.classList.remove('collapsed');
         } else {
           e.target.parentElement.nextElementSibling.classList.add('collapsed');
+        }
+        // 箭头
+        const parentElement = e.target.parentElement;
+        if (parentElement && parentElement.classList.contains('log-console-item-msg')) {
+          parentElement.childNodes.forEach(node => {
+            if (node.tagName === 'SPAN' && node.classList.contains('log-console-item-msg-arrow')) {
+              if (node.classList.contains('collapsed')) {
+                node.classList.remove('collapsed');
+              } else {
+                node.classList.add('collapsed');
+              }
+            }
+          })
         }
       }
     })
